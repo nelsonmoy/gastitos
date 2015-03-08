7e0aad2a742077b92df7873c275383a2f46d6013
Nelson Manuel Ordaz Yglesias <nelsonmanuel@outlook.com>
2015-03-08 09:28:37 -0600

    Limpiando configuracion

--- back/config.php
+++ back/config.php
@@ -1,14 +1,8 @@
 <?php
 
 	header("Access-Control-Allow-Origin: *");
-	/*
-	$dbUser = "motmotsy_gastito";
-	$db = "motmotsy_gastitos";
-	$dbPassword = "{lDN}9Vy,knx";
-	$dbHost = "localhost";
-	*/
 
-	$dbUser = "a1972699_gastito";
-	$db = "a1972699_gastito";
-	$dbPassword = "asd123GASTITOS";
-	$dbHost = "mysql7.000webhost.com";
+	$dbUser = "";
+	$db = "";
+	$dbPassword = "";
+	$dbHost = "";
